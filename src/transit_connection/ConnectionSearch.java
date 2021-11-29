8f469318e771be9a7b3538b050c8e0168485d384
Lima239 <j.lichmanova@gmail.com>
2021-11-29 22:49:31 +0100

    updating datatypes

--- src/transit_connection/ConnectionSearch.java
+++ src/transit_connection/ConnectionSearch.java
@@ -1,4 +1,20 @@
 package transit_connection;
 
+import java.util.*;
+
 public class ConnectionSearch {
+    private Stops stops;
+    private Lines lines;
+
+    public ConnectionSearch(DataLoaderInterface dataLoader) {
+        this.stops = new Stops(dataLoader);
+        this.lines = new Lines(dataLoader);
+    }
+
+    public ConnectionData search(StopName from, StopName to, Time time) {
+        stops.setStartingStop(from, time);
+        List<LineName> from_lines = stops.getLines(from);
+        lines.updateReachable(from_lines, from, time);
+        return new ConnectionData();
+    }
 }
