fc9346fbec440a59182293ab0baf57a0dd1e6446
Lima239 <j.lichmanova@gmail.com>
2021-11-29 22:38:36 +0100

    DataLoaderFactory interface

---
+++ src/transit_connection/DataLoaderFactory.java
@@ -0,0 +1,9 @@
+package transit_connection;
+
+import java.util.List;
+public interface DataLoaderFactory {
+
+    public List<Line> loadLines(StopName stop);
+    public Line loadLine(LineName line);
+    public Stop loadStop(StopName stop);
+}
