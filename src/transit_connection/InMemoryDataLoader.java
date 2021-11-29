805057e8b81a82d7ed68283a32f9372d7a851907
Lima239 <j.lichmanova@gmail.com>
2021-11-29 22:40:39 +0100

    DataLoader implementation

---
+++ src/transit_connection/InMemoryDataLoader.java
@@ -0,0 +1,13 @@
+package transit_connection;
+
+public class InMemoryDataLoader implements DataLoaderInterface{
+    @Override
+    public Stop loadStop(StopName stop) {
+        return null;
+    }
+
+    @Override
+    public Line loadLine(LineName line) {
+        return null;
+    }
+}
