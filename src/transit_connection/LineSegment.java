804b611c0717beca4974d249f3d2bf1f96055f1d
Lima239 <j.lichmanova@gmail.com>
2021-11-30 18:28:17 +0100

    creating line segment

--- src/transit_connection/LineSegment.java
+++ src/transit_connection/LineSegment.java
@@ -1,4 +1,40 @@
 package transit_connection;
 
+import java.util.*;
+
 public class LineSegment {
+    private TimeDiff timeToNextStop;
+    private Map<Time, Integer> numberOfPassengers;
+    private int capacity;
+    private LineName lineName;
+    private Stop nextStop;
+
+    public LineSegment(TimeDiff timeToNextStop, int capacity, LineName lineName, Stop nextStop){
+        this.timeToNextStop = timeToNextStop;
+        this.capacity = capacity;
+        this.lineName = lineName;
+        this.nextStop = nextStop;
+        numberOfPassengers = new HashMap<>();
+    }
+
+    public Dictionary<Time, StopName> nextStop(Time startTime){
+        Dictionary<Time, StopName> tmp = new Hashtable();
+        tmp.put(new Time(startTime.getTime().plus(timeToNextStop.getDifference())), nextStop.getName());
+        return tmp;
+    }
+
+    public Tuple nextStopAndUpdateReachable(Time startTime){
+        if (!numberOfPassengers.containsKey(startTime)){
+            numberOfPassengers.put(startTime,0);
+        }
+        boolean enoughSpace = numberOfPassengers.get(startTime) < capacity;
+        if(enoughSpace) nextStop.updateReachableAt(new Time(startTime.getTime().plus(timeToNextStop.getDifference())),
+                Optional.ofNullable(lineName));
+        return new Tuple(new Time(startTime.getTime().plus(timeToNextStop.getDifference())), nextStop.getName(),
+                enoughSpace);
+    }
+
+    public void incrementCapacity(Time startingTime){
+        numberOfPassengers.put(startingTime, numberOfPassengers.get(startingTime) + 1);
+    }
 }
