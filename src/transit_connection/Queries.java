f2c1750b4395efd7299e8d9ccdd59a68dda2a520
Lima239 <j.lichmanova@gmail.com>
2021-11-30 18:30:19 +0100

    creating queries class

---
+++ src/transit_connection/Queries.java
@@ -0,0 +1,38 @@
+package transit_connection;
+
+import java.sql.*;
+
+public class Queries {
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void selectAll(){
+        String sql = "SELECT id, name, capacity FROM warehouses";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
