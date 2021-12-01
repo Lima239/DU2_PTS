d2c999dfe010e27081e9b9655f68de21732442c6
Lima239 <j.lichmanova@gmail.com>
2021-12-01 23:54:00 +0100

    some updates of methods

--- src/transit_connection/ConnectionData.java
+++ src/transit_connection/ConnectionData.java
@@ -1,5 +1,16 @@
 package transit_connection;
 
-public class ConnectionData {
+import java.util.List;
+import java.util.Map;
 
+public class ConnectionData {
+    private List<Map.Entry<StopName, Time>> route;
+
+    public List<Map.Entry<StopName, Time>> getRoute() { return route; }
+
+    public Time getOverallTime() {
+        if (route.isEmpty())
+            return null;
+        return route.get(route.size() - 1).getValue();
+    }
 }
