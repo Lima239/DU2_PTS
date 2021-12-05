100554e22bd5994dbfdd0f02ac6da1503af9e1fa
Lima239 <j.lichmanova@gmail.com>
2021-12-05 15:28:37 +0100

    update of loadLine method

--- src/transit_connection/InMemoryDataLoader.java
+++ src/transit_connection/InMemoryDataLoader.java
@@ -1,13 +1,29 @@
 package transit_connection;
 
+import java.util.*;
+
 public class InMemoryDataLoader implements DataLoaderInterface{
+    private Map<StopName, List<LineName>> stopsInMemory;
+    private Map<LineName, Dictionary<ArrayList<Time>, StopName>> linesInMemory;
+    private Map<LineName, ArrayList<LineSegmentInterface>> lineSegmentsInMemory;
+
+    public InMemoryDataLoader(Map<StopName, List<LineName>> stopsInMemory, Map<LineName, Dictionary<ArrayList<Time>, StopName>> linesInMemory,
+        Map<LineName, ArrayList<LineSegmentInterface>> lineSegmentsInMemory){
+        this.stopsInMemory = stopsInMemory;
+        this.lineSegmentsInMemory = lineSegmentsInMemory;
+        this.linesInMemory = linesInMemory;
+    }
+
     @Override
     public Stop loadStop(StopName stop) {
-        return null;
+        if (!stopsInMemory.containsKey(stop)) return null;
+        return new Stop(stop, stopsInMemory.get(stop));
     }
 
     @Override
     public Line loadLine(LineName line) {
-        return null;
+        if (!linesInMemory.containsKey(line)) return null;
+        Dictionary<ArrayList<Time>,StopName> lineData = linesInMemory.get(line);
+        return new Line(line, lineData.keys().nextElement(), lineData.elements().nextElement(), lineSegmentsInMemory.get(line));
     }
 }
