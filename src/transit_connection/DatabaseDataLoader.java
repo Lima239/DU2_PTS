cbaac9b278f5dd3b0ffa582b4284daea82876735
Lima239 <j.lichmanova@gmail.com>
2021-12-05 15:24:34 +0100

    some updates

---
+++ src/transit_connection/DatabaseDataLoader.java
@@ -0,0 +1,18 @@
+package transit_connection;
+
+public class DatabaseDataLoader implements DBInterface{
+    private DBInterface database;
+
+    /*public DatabaseDataLoader(DBInterface database, ){
+
+    }
+
+    public Stop loadStop(StopName stop) {
+
+    }
+
+    public Line loadLine(LineName line) {
+
+    }*/
+
+}
