0082ccc5c7d48d7baa4edfef87c3eb8d7f418669
Lima239 <j.lichmanova@gmail.com>
2021-12-07 23:06:32 +0100

    update of setUp method

--- src/test/InMemoryIntegrationTest.java
+++ src/test/InMemoryIntegrationTest.java
@@ -2,11 +2,11 @@ package test;
 
 import org.junit.Test;
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import transit_connection.*;
 import java.time.LocalTime;
 import java.util.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class InMemoryIntegrationTest {
     private final ArrayList<Time> startingTimes = (ArrayList<Time>) List.of(new Time(LocalTime.of(10,10)),
@@ -19,23 +19,34 @@ public class InMemoryIntegrationTest {
             Map.entry(new StopName("stop X"), List.of()),
             Map.entry(new StopName("stop Y"), List.of())
             );
-    private Map<ArrayList<Time>, StopName> tmp = new HashMap<>();
-
-
-    private Map<LineName, Dictionary<ArrayList<Time>, StopName>> linesInMemory =  Map.of(
-            new LineName("linka1"),new HashMap<ArrayList<Time>, StopName>(startingTimes, new StopName("Stop A")),
-            new LineName("linka2"), new HashMap<ArrayList<Time>, StopName>(startingTimes, new StopName("Stop C")));
-
-
+    private Dictionary<ArrayList<Time>, StopName> tmp1 = new Hashtable();
+    private Dictionary<ArrayList<Time>, StopName> tmp2 = new Hashtable();
+    private Map<LineName, Dictionary<ArrayList<Time>, StopName>> linesInMemory;
+    private ConnectionSearch connectionSearch;
     private Map<LineName, ArrayList<LineSegmentInterface>> lineSegmentsInMemory;
+    private DataLoaderInterface dataLoader;
+
 
     @Before
     public void setUp() {
-
-        tmp.put(startingTimes, new StopName("Stop A"));
-        tmp.put(startingTimes, new StopName("Stop C"));
+        tmp1.put(startingTimes, new StopName("Stop A"));
+        tmp2.put(startingTimes, new StopName("Stop C"));
         linesInMemory = Map.of(
-                new LineName("linka1"), tmp.get(0),
-                new LineName("linka2"), new HashMap<ArrayList<Time>, StopName>(startingTimes, new StopName("Stop C")));
-        }
+                new LineName("linka1"), tmp1,
+                new LineName("linka2"), tmp2);
+
+        dataLoader = new InMemoryDataLoader(stopsInMemory, linesInMemory, lineSegmentsInMemory);
+        StopsInterface stops = new Stops(dataLoader);
+        LinesInterface lines = new Lines(dataLoader);
     }
+
+    @Test
+    public void dataLoaderTest(){
+        assertEquals(new Stop(new StopName("stop A"), List.of(new LineName("linka1"), new LineName("linka2"))).getName()
+                , dataLoader.loadStop(new StopName("stop A")).getName());
+        assertEquals(new Stop(new StopName("stop B"), List.of(new LineName("linka1"))).getName()
+                , dataLoader.loadStop(new StopName("stop B")).getName());
+        assertEquals(new Stop(new StopName("stop C"), List.of(new LineName("linka1"), new LineName("linka2"))).getName()
+                , dataLoader.loadStop(new StopName("stop C")).getName());
+    }
+}
