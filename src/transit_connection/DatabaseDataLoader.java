e6114ff4d2c568afbb27e828c54809a815ccd587
Lima239 <j.lichmanova@gmail.com>
2021-12-07 18:01:33 +0100

    change of loading data from db

--- src/transit_connection/DatabaseDataLoader.java
+++ src/transit_connection/DatabaseDataLoader.java
@@ -1,18 +1,52 @@
 package transit_connection;
 
+import java.sql.*;
+
 public class DatabaseDataLoader implements DBInterface{
     private DBInterface database;
 
-    /*public DatabaseDataLoader(DBInterface database, ){
-
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
     }
 
-    public Stop loadStop(StopName stop) {
+    public void loadLineAndFirstStop(LineName line){
+        String sql = "SELECT LineName, ID, StartingStop" +
+                "FROM LineIDS " +
+                "WHERE LineName = " + line.toString();
 
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public Line loadLine(LineName line) {
+    public void loadStop(StopName stopName){
+        String sql = "SELECT StartingStop FROM LineIDS" +
+                "WHERE StartingStop = " + stopName.toString();
 
-    }*/
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
